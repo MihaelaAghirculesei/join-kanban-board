de9287846bfcd2c2d0b8533ad1b339fbe7453019
Marvin Schneemann <marvinschneemann@gmx.de>
2025-04-13 08:35:08 +0200

    changes router-outlet and folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContactComponent } from './main-content/contact/contact.component';
-import { ContactOverlayComponent } from './main-content/contact/contact-overlay/contact-overlay.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,ContactComponent, HeaderComponent, SidebarComponent ],
+  imports: [CommonModule, RouterOutlet,HeaderComponent, SidebarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
